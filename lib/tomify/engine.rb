dc3320e95da77ca02cb74116fa4493869441c2d9
Tom Prats <tprats108@gmail.com>
2017-03-28 20:37:12 -0400

    Gem Dependencies

--- lib/tomify/engine.rb
+++ lib/tomify/engine.rb
@@ -1,5 +1,10 @@
 require "rake"
 
+skip = ["fog-aws"]
+Gem.loaded_specs["tomify"].dependencies.each do |d|
+  require d.name unless d.name.in? skip
+end
+
 module Tomify
   class Engine < ::Rails::Engine
     rake_tasks do
